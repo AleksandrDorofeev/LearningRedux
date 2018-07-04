493e4a66649f0dcedd9e2ef951df63cd6921af6d
Dorofeev Aleksandr <dorofeevaleksandr@Dorofeev-Aleksandr.local>
2018-07-04 17:59:00 +0300

    react-redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,35 +5,46 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+    // state = {
+    //     counter: 0
+    // }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+    // counterChangedHandler = ( action, value ) => {
+    //     switch ( action ) {
+    //         case 'inc':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    //             break;
+    //         case 'dec':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+    //             break;
+    //         case 'add':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+    //             break;
+    //         case 'sub':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+    //             break;
+    //     }
+    // }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.cntr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter}  />
+                <hr />
+                <button onClick={this.props.onAddResult}>Result</button>
+                <ul>
+                    {this.props.res.map(res => {
+                        console.log(res);
+                        return (
+                            <li key={res.id} onClick={() => this.props.onDeleteResult(res.id)}>{res.value}</li>
+                        )    
+                    })}
+                    {/* <li onClick={this.props.onDeleteResult}>{this.props.res}</li> */}
+                </ul>
             </div>
         );
     }
@@ -41,13 +52,19 @@ class Counter extends Component {
 
 const mapStateTOProps = state => {
     return {
-        cntr: state.counter
+        cntr: state.counter,
+        res: state.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: "DECREMENT"}),
+        onAddCounter: () => dispatch({type: "ADD", val: 5}),
+        onSubstractCounter: () => dispatch({type: "SUBSCTRACT", val: 5}),
+        onAddResult: () => dispatch({type: "ADD_RESULT"}),
+        onDeleteResult: (id) => dispatch({type: "DELETE_RESULT", filteredId: id})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,69 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
-  if(action.type ==="INCREMENT") {
+  switch(action.type) {
+    case "INCREMENT": 
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    case "DECREMENT": 
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case "ADD": 
+      return {
+        ...state,
+        counter: state.counter + action.val
+      }
+    case "SUBSCTRACT": 
+      return {
+        ...state,
+        counter: state.counter - action.val
+      }
+    case "ADD_RESULT":
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter + " "})
+      }  
+    case "DELETE_RESULT":
+    const updatedArray = state.results.filter(result => result.id !== action.filteredId)
     return {
       ...state,
-      counter: state.counter + 1
+      results: updatedArray
     }
   }
-  return state
+  return state 
+  
 }
+  // if(action.type === "INCREMENT") {
+  //   return {
+  //     ...state,
+  //     counter: state.counter + 1
+  //   }
+  // }
+  // if(action.type === "DECREMENT") {
+  //   return {
+  //     ...state,
+  //     counter: state.counter - 1
+  //   }
+  // }
+  // if(action.type === "ADD") {
+  //   return {
+  //     ...state,
+  //     counter: state.counter + action.val
+  //   }
+  // }
+  // if(action.type === "SUBSCTRACT") {
+  //   return {
+  //     ...state,
+  //     counter: state.counter - action.val
+  //   }
+  // }
+  // return state
 
 export default reducer;
